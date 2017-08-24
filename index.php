b51adcbadcfb4c56d811c66d77bdb45383b7671b
Katdelorme <katdelorme@yahoo.com>
2017-08-24 08:32:32 -0400

    layingout and styling the home page. salonist, discover and style-design sections. Drop cap for discover, flex for styles. Minor star of footer

--- index.php
+++ index.php
@@ -5,24 +5,79 @@ include ('./includes/header.html');
 
 	<section class="hometop">
 		<div class="hometopContent">
-			<h5>Hello World</h5>
+			<h2>Concept Name Here</h2>
+			<h4>Subtitle Here</h4>
+			<p>A question to ask the audience?
+			<br/>A statment regarding the question or answer.</p>
 		</div>
 	</section>
 
 	<section>
 		<div class="threeFourth">
+			<div class="contentTip">
 			<div class="forty">
 				<img src="assets/owners.jpg" alt="Owners: Janica Southwick and Ryutaro Kashima" />
 			</div>
 			<div class="sixty">
 				<h2>Two Salonists Coming Together</h2>
-				<p>Stuff</p>
+				<p>Sed sed semper elit. Donec interdum tristique justo, a placerat dui posuere imperdiet. Donec turpis leo, rutrum vitae posuere vel, varius vitae velit. Integer eu posuere mauris, sed tincidunt odio. Integer eros erat, rutrum id nunc id, tempor accumsan nunc. </p>
+				<div class="logosTwo">
+					<img src="assets/logoJoliNails.png" alt="JoliNails logo" />
+					<img src="assets/logoVenusrico.png" alt="VenusRico Logo" />
+				</div>
+			</div>
 			</div>
 		</div>
 		<div class="oneFourth">
 			<h1>Book An Appointment</h1>
 			<a href="#" class="btn">Book Now</a>
 		</div>
+	</section>
+
+	<section>
+		<div class="thirty">
+		</div>
+		<div class="seventy">
+			<h1>Discover Diamond Nails</h1>
+			<h3>What it is and how they’re placed</h3>
+			<hr>
+			<div class="textColumn">
+				<p>Diamond nails are the most luxurious nail treatment in the world. High quality diamonds of all colors are applied directly to the nail bed, with gel and a patented application method. Diamonds are re-applicable so clients may use them for many treatments. Customers feel assured with a warranty for diamonds that come
+loose or are lost.</p>
+ 				<p>Nullam pretium quis ipsum non tristique. Pellentesque dapibus, nisl a iaculis dictum, mauris ex dapibus ex, at tincidunt augue purus ut dolor. Maecenas tristique turpis sed magna viverra ultricies. In commodo, justo ut tincidunt ornare, justo urna lobortis nisl, ut suscipit velit nunc venenatis urna.</p>
+				<p>Sed sed semper elit. Donec interdum tristique justo, a placerat dui posuere imperdiet. Donec turpis leo, rutrum vitae posuere vel, varius vitae velit. Integer eu posuere mauris, sed tincidunt odio. Integer eros erat, rutrum id nunc id, tempor accumsan nunc.</p>
+			</div>
+		</div>
+	</section>
+
+	<section class="verticalSpace">
+		<div class="columnThree">
+			<div>
+				<h4>Fashion Nail</h4>
+				<p>Integer eros erat, rutrum id nunc id, tempor accumsan nunc. Donec ut enim fermentum, consequat mi id, mattis arcu. Vivamus ac nisi interdum, porttitor.</p>
+			</div>
+			<div>
+				<h4>Ita nail</h4>
+				<p>Ita - Nail is a specialized Japanese hand-painted art which uses the nail bed as a canvas.</p>
+			</div>
+			<div>
+				<h4>Diamond Nail</h4>
+				<p>Integer eros erat, rutrum id nunc id, tempor accumsan nunc. Donec ut enim fermentum, consequat mi id, mattis arcu. Vivamus ac nisi interdum, porttitor.</p>
+			</div>
+		</div>
+		<div class="featuredStyle">
+			<div class="featuredStyleL">
+				<h1>Minimalistic</h1>
+				<p>Integer eros erat, rutrum id nunc id, tempor accumsan nunc. Donec ut enim fermentum, consequat mi id, mattis arcu. Vivamus ac nisi interdum, porttitor. Integer eros erat, rutrum id nunc id, tempor accumsan nunc. Donec ut enim fermentum, consequat mi id, mattis arcu. Vivamus ac nisi interdum, porttitor.</p>
+				<a href="#" class="btn">Select Your Style</a>
+			</div>
+			<div class="featuredStyleR">
+				<!--<img src="assets/venuesGemsBrownLarge.jpg" alt="Example nails. A set of brown nails with jems on top" />-->
+			</div>
+		</div>
+	</section>
+
+	<section class="newsletter">
 	</section>
 
 <?php
