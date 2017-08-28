cb158787750e49535cc5c62787dde00e219597dd
Katdelorme <katdelorme@yahoo.com>
2017-08-28 08:27:57 -0400

    impliment bootstrap, start contact page

---
+++ contact.php
@@ -0,0 +1,44 @@
+<?php
+$page_title='Contact - LUXITA';
+$page_desc='';
+$page_keywords="Contact LUXITA, contact Luxita, how to schedule a nail appointment in Luxita, schedule nail appointment, Luxita phone number, Luxita address, Luxita email, where is Luxita, address of Luxita";
+$facebook_img="";
+$twitter_img="";
+include ('./includes/header.html');
+/* PAGE CONTENT STARTS HERE! */
+?>
+
+	<div class="container-fluid">
+		<section class="section">
+			<h1>Get In Contact</h1>
+			<p>Sed sed semper elit. Donec interdum tristique justo, a placerat dui posuere imperdiet. Donec turpis leo, rutrum vitae posuere vel, varius vitae velit. Integer eu posuere mauris, sed tincidunt odio. Integer eros erat, rutrum id nunc id, tempor accumsan nunc.</p>
+			<a href="#" class="btn">Book Now</a>
+		</section>
+
+		<section class="section">
+			<div class="row">
+				<div class="col-lg-8 col-md-8 col-xs-12">
+					<h1>Send Us A Message</h1>
+					<h3>Have any questions? Fill the form below.</h3>
+					<hr>
+					<form action="#">
+								<input type="text" value="" name="FNAME" placeholder="First Name">
+								<input type="email" value="" name="EMAIL" class="required email" placeholder="Email Address">
+								<input type="submit" value="Stay in the Know" name="subscribe">
+					</form>
+				</div>
+				<div class="col-lg-4 col-md-8 col-xs-12" style="background-color:#edece8;">
+					<p>Sed sed semper elit. Donec interdum tristique justo, a placerat dui posuere imperdiet. Donec turpis leo, rutrum vitae.</p>
+					<p>Marunouchi Trust Tower
+					<br>Main, 1-8-3 Marunouchi Chiyoda-ku,
+					<br>Tokyo, 100-8283, Japan</p>
+					<p>T:(81 3) 5555 5555 </p>
+				</div>
+			</div>
+		</section>
+	</div>
+
+<?php
+/* PAGE CONTENT ENDS HERE! */
+require ('./includes/footer.html');
+?>
