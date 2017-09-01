376707cf2f2688a235b4ec6ccf94d2a0390e3153
Katdelorme <katdelorme@yahoo.com>
2017-09-01 00:46:20 -0400

    responsiveness prior day

--- index.php
+++ index.php
@@ -4,103 +4,112 @@ include ('./includes/header.html');
 ?>
 
   <div id="fullpage">
-	<section class="section hometop">
-		<div class="hometopContent">
-			<h2>Concept Name Here</h2>
-			<h4>Subtitle Here</h4>
-			<p>A question to ask the audience?
-			<br/>A statment regarding the question or answer.</p>
-		</div>
-	</section>
+	<div class="container-fluid">
 
-	<section class="section">
-		<div class="threeFourth">
-			<div class="contentTip">
-			<div class="forty">
-				<img src="assets/owners.jpg" alt="Owners: Janica Southwick and Ryutaro Kashima" />
+		<section class="section hometop">
+			<div class="hometopContent">
+				<h2>Concept Name Here</h2>
+				<h4>Subtitle Here</h4>
+				<p>A question to ask the audience?
+				<br/>A statment regarding the question or answer.</p>
 			</div>
-			<div class="sixty">
-				<h2>Two Salonists Coming Together</h2>
-				<p>Sed sed semper elit. Donec interdum tristique justo, a placerat dui posuere imperdiet. Donec turpis leo, rutrum vitae posuere vel, varius vitae velit. Integer eu posuere mauris, sed tincidunt odio. Integer eros erat, rutrum id nunc id, tempor accumsan nunc. </p>
-				<div class="logosTwo">
-					<img src="assets/logoJoliNails.png" alt="JoliNails logo" />
-					<img src="assets/logoVenusrico.png" alt="VenusRico Logo" />
+		</section>
+
+		<section class="section">
+			<div class="threeFourth row">
+				<div class="contentTip">
+				<div class="forty">
+					<img src="assets/owners.jpg" alt="Owners: Janica Southwick and Ryutaro Kashima" />
+				</div>
+				<div class="sixty">
+					<h2>Two Salonists Coming Together</h2>
+					<p>Sed sed semper elit. Donec interdum tristique justo, a placerat dui posuere imperdiet. Donec turpis leo, rutrum vitae posuere vel, varius vitae velit. Integer eu posuere mauris, sed tincidunt odio. Integer eros erat, rutrum id nunc id, tempor accumsan nunc. </p>
+					<div class="logosTwo">
+						<img src="assets/logoJoliNails.png" alt="JoliNails logo" />
+						<img src="assets/logoVenusrico.png" alt="VenusRico Logo" />
+					</div>
+				</div>
 				</div>
 			</div>
-			</div>
-		</div>
-		<div class="oneFourth">
-			<h1>Book An Appointment</h1>
-			<a href="#" class="btn">Book Now</a>
-		</div>
-	</section>
-
-	<section  class="section">
-		<div class="thirty">
-		</div>
-		<div class="seventy">
-			<h1>Discover Diamond Nails</h1>
-			<h3>What it is and how they’re placed</h3>
-			<hr>
-			<div class="textColumn">
-				<p>Diamond nails are the most luxurious nail treatment in the world. High quality diamonds of all colors are applied directly to the nail bed, with gel and a patented application method. Diamonds are re-applicable so clients may use them for many treatments. Customers feel assured with a warranty for diamonds that come
-loose or are lost.</p>
- 				<p>Nullam pretium quis ipsum non tristique. Pellentesque dapibus, nisl a iaculis dictum, mauris ex dapibus ex, at tincidunt augue purus ut dolor. Maecenas tristique turpis sed magna viverra ultricies. In commodo, justo ut tincidunt ornare, justo urna lobortis nisl, ut suscipit velit nunc venenatis urna.</p>
-				<p>Sed sed semper elit. Donec interdum tristique justo, a placerat dui posuere imperdiet. Donec turpis leo, rutrum vitae posuere vel, varius vitae velit. Integer eu posuere mauris, sed tincidunt odio. Integer eros erat, rutrum id nunc id, tempor accumsan nunc.</p>
-			</div>
-		</div>
-	</section>
-
-	<section class="section verticalSpace">
-		<div class="columnThree">
-			<div>
-				<h4>Fashion Nail</h4>
-				<p>Integer eros erat, rutrum id nunc id, tempor accumsan nunc. Donec ut enim fermentum, consequat mi id, mattis arcu. Vivamus ac nisi interdum, porttitor.</p>
-			</div>
-			<div>
-				<h4>Ita nail</h4>
-				<p>Ita - Nail is a specialized Japanese hand-painted art which uses the nail bed as a canvas.</p>
-			</div>
-			<div>
-				<h4>Diamond Nail</h4>
-				<p>Integer eros erat, rutrum id nunc id, tempor accumsan nunc. Donec ut enim fermentum, consequat mi id, mattis arcu. Vivamus ac nisi interdum, porttitor.</p>
-			</div>
-		</div>
-		<div class="featuredStyle">
-			<div class="featuredStyleL">
-				<h1>Minimalistic</h1>
-				<p>Integer eros erat, rutrum id nunc id, tempor accumsan nunc. Donec ut enim fermentum, consequat mi id, mattis arcu. Vivamus ac nisi interdum, porttitor. Integer eros erat, rutrum id nunc id, tempor accumsan nunc. Donec ut enim fermentum, consequat mi id, mattis arcu. Vivamus ac nisi interdum, porttitor.</p>
-				<a href="#" class="btn">Select Your Style</a>
-			</div>
-			<div class="featuredStyleR">
-				<!--<img src="assets/venuesGemsBrownLarge.jpg" alt="Example nails. A set of brown nails with jems on top" />-->
-			</div>
-		</div>
-	</section>
-
-	<section class="section newsletter" data-centered="true">
-		<h1>Subscribe</h1>
-		<h5>Be the first to know about our launch</h5>
-		<!-- Begin MailChimp Signup Form -->
-		<div id="mc_embed_signup">
-		<form action="#" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-			<div id="mc_embed_signup_scroll">
-					<input type="text" value="" name="FNAME" class="" id="mce-FNAME" placeholder="First Name">
-					<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL" placeholder="Email Address">
-				<div id="mce-responses" class="clear">
-					<div class="response" id="mce-error-response" style="display:none"></div>
-					<div class="response" id="mce-success-response" style="display:none"></div>
-				</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-				<div style="position: absolute; left: -5000px;"><input type="text" name="b_7aca85dad056373aad529face_d3753f5f82" tabindex="-1" value=""></div>
-				<div class="clear">
-					<input type="submit" value="Stay in the Know" name="subscribe">
+			<div class="oneFourth row">
+					<div class="col-md-9 col-xs-12">
+				<h1>Book An Appointment</h1>
+				</div>
+				<div class="col-md-3 col-xs-12">
+					<a href="#" class="btn">Book Now</a>
 				</div>
 			</div>
-		</form>
-		</div>
-		<!--End mc_embed_signup-->
-	</section>
-</div><!--fullpage close-->
+		</section>
+
+		<section  class="section">
+			<div class="row">
+				<div class="thirty col-md-4 col-xs-12">
+				</div>
+				<div class="seventy col-md-8 col-xs-12">
+					<h1>Discover Diamond Nails</h1>
+					<h3>What it is and how they’re placed</h3>
+					<hr>
+					<div class="textColumn">
+						<p>Diamond nails are the most luxurious nail treatment in the world. High quality diamonds of all colors are applied directly to the nail bed, with gel and a patented application method. Diamonds are re-applicable so clients may use them for many treatments. Customers feel assured with a warranty for diamonds that come
+		loose or are lost.</p>
+		 				<p>Nullam pretium quis ipsum non tristique. Pellentesque dapibus, nisl a iaculis dictum, mauris ex dapibus ex, at tincidunt augue purus ut dolor. Maecenas tristique turpis sed magna viverra ultricies. In commodo, justo ut tincidunt ornare, justo urna lobortis nisl, ut suscipit velit nunc venenatis urna.</p>
+						<p>Sed sed semper elit. Donec interdum tristique justo, a placerat dui posuere imperdiet. Donec turpis leo, rutrum vitae posuere vel, varius vitae velit. Integer eu posuere mauris, sed tincidunt odio. Integer eros erat, rutrum id nunc id, tempor accumsan nunc.</p>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<section class="section verticalSpace">
+			<div class="columnThree row">
+				<div class="col-md-4 col-xs-12">
+					<h4>Fashion Nail</h4>
+					<p>Integer eros erat, rutrum id nunc id, tempor accumsan nunc. Donec ut enim fermentum, consequat mi id, mattis arcu. Vivamus ac nisi interdum, porttitor.</p>
+				</div>
+				<div class="col-md-4 col-xs-12">
+					<h4>Ita nail</h4>
+					<p>Ita - Nail is a specialized Japanese hand-painted art which uses the nail bed as a canvas.</p>
+				</div>
+				<div class="col-md-4 col-xs-12">
+					<h4>Diamond Nail</h4>
+					<p>Integer eros erat, rutrum id nunc id, tempor accumsan nunc. Donec ut enim fermentum, consequat mi id, mattis arcu. Vivamus ac nisi interdum, porttitor.</p>
+				</div>
+			</div>
+			<div class="featuredStyle row">
+				<div class="featuredStyleL col-md-5 col-xs-12">
+					<h1>Minimalistic</h1>
+					<p>Integer eros erat, rutrum id nunc id, tempor accumsan nunc. Donec ut enim fermentum, consequat mi id, mattis arcu. Vivamus ac nisi interdum, porttitor. Integer eros erat, rutrum id nunc id, tempor accumsan nunc. Donec ut enim fermentum, consequat mi id, mattis arcu. Vivamus ac nisi interdum, porttitor.</p>
+					<a href="#" class="btn">Select Your Style</a>
+				</div>
+				<div class="featuredStyleR col-md-7 col-xs-12">
+					<!--<img src="assets/venuesGemsBrownLarge.jpg" alt="Example nails. A set of brown nails with jems on top" />-->
+				</div>
+			</div>
+		</section>
+
+		<section class="section newsletter basepadding" data-centered="true">
+					<h1>Subscribe</h1>
+					<h5>Be the first to know about our launch</h5>
+					<!-- Begin MailChimp Signup Form -->
+					<div id="mc_embed_signup">
+					<form action="#" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+						<div id="mc_embed_signup_scroll">
+								<input type="text" value="" name="FNAME" class="" id="mce-FNAME" placeholder="First Name">
+								<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL" placeholder="Email Address">
+							<div id="mce-responses" class="clear">
+								<div class="response" id="mce-error-response" style="display:none"></div>
+								<div class="response" id="mce-success-response" style="display:none"></div>
+							</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+							<div style="position: absolute; left: -5000px;"><input type="text" name="b_7aca85dad056373aad529face_d3753f5f82" tabindex="-1" value=""></div>
+							<div class="clear">
+								<input type="submit" value="Stay in the Know" name="subscribe">
+							</div>
+						</div>
+					</form>
+					</div>
+					<!--End mc_embed_signup-->
+		</section>
+	</div><!--container close-->
+	</div><!--fullpage close-->
 
 
 
---
+++ photo-rights.php
@@ -0,0 +1,36 @@
+<?php
+$page_title='Privacy Policy - LUXITA';
+$page_desc='';
+$page_keywords="";
+$facebook_img="";
+$twitter_img="";
+include ('./includes/header.html');
+/* PAGE CONTENT STARTS HERE! */
+?>
+
+	<div class="container-fluid policy">
+
+		<div class="basepadding">
+			<div class="row">
+				<div class="col-xs-12">
+					<h1 class="text-center">Photo Rights</h1>
+					<p>Integer eros erat, rutrum id nunc id, tempor accumsan nunc. Donec ut enim fermentum, consequat mi id, mattis arcu. Vivamus ac nisi interdum, porttitor. Integer eros erat, rutrum id nunc id, tempor accumsan nunc. Donec ut enim fermentum, consequat mi id, mattis arcu. Vivamus ac nisi interdum, porttitor.</p>
+
+					<h6>Main Point</h6>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed consectetur ligula in placerat malesuada. Nulla egestas dui porttitor ipsum sodales tincidunt. Aliquam rutrum elit ullamcorper nulla viverra condimentum. Quisque aliquet tortor ut risus placerat, vel ullamcorper lorem pretium. Interdum et malesuada fames ac ante ipsum primis in faucibus. Donec ullamcorper dui quis felis sagittis tincidunt. Donec vitae felis quis ex luctus dictum vitae eget dolor.</p>
+
+					<h6>Main Point</h6>
+					<p>Nunc aliquet rhoncus nunc commodo commodo. Cras sit amet dictum neque, vitae blandit sapien. Nulla ac tortor vitae mi viverra posuere. Duis elit mauris, auctor id elit at, congue venenatis dolor. Donec auctor velit venenatis pharetra aliquam. Nunc feugiat ullamcorper diam, a pellentesque lacus pretium vel. Quisque consectetur magna lorem, eget aliquet dolor dapibus et. Nullam eget rhoncus libero, id fermentum lectus. Phasellus luctus elit eget pretium facilisis. Praesent dictum dolor at mi convallis lacinia at sed purus. Fusce placerat diam vel dolor congue aliquet. Fusce aliquet ligula sed enim maximus, id ultrices ex aliquet. Morbi purus justo, suscipit ac lorem id, ullamcorper convallis enim. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+
+					<h6>Main Point</h6>
+					<p>Donec feugiat iaculis nisl quis viverra. Cras quis egestas magna. Donec tincidunt mi eu sagittis tincidunt. Nam pretium lectus velit, in cursus neque lobortis ac. Phasellus porttitor lorem nisi. Suspendisse eleifend sit amet purus sit amet sollicitudin. Nullam dolor sapien, auctor tempus consequat nec, interdum ac tellus. Sed mattis nunc vitae interdum rhoncus. Nulla facilisi. Nulla vel fringilla nunc. Quisque eget mauris mi. Mauris feugiat, urna id ullamcorper sagittis, nisi erat aliquam nulla, sed blandit nunc quam vitae enim. Nunc elementum, mi eget hendrerit pulvinar, lorem nisl lacinia lacus, nec pellentesque mauris tortor vel urna. Praesent augue quam, pulvinar at nisl vel, sodales porta lectus. Nam nulla nibh, bibendum eu blandit ut, ultrices sit amet risus. Nam id enim id dui suscipit pharetra.</p>
+				</div>
+			</div>
+		</div>
+
+	</div><!--container close-->
+
+<?php
+/* PAGE CONTENT ENDS HERE! */
+require ('./includes/footer.html');
+?>
