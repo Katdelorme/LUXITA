1371cd27320e71450ffb5d2daa6a45fea1a38d6f
Katdelorme <katdelorme@yahoo.com>
2017-09-26 01:02:53 -0400

    images added to discover diamond and responsive cover image

--- discoverdiamond.php
+++ discoverdiamond.php
@@ -23,51 +23,16 @@ include ('./includes/header.html');
 
 		<section class="section greybg">
 			<div class="row">
-      <!--s<div class="page-container">
-      <div class="main-container">
-        <div class="inner-main-container">
-          <div class="collage-container">
-            <div style="background: url('http://i.huffpost.com/gen/1647117/images/n-POKEMON-large570.jpg');" class="photo-tile half single"></div>
-            <div style="background: url('http://www.wall321.com/thumbnails/detail/20130608/nintendo%20abstract%20minimalistic%20mario%20princess%20mario%20bros%20super%20mario%20luigi%20little%20miss%20sunshine%20prin_www.wall321.com_99.jpg');" class="photo-tile half single"></div>
-            <div style="background: url('http://www.topdesignmag.com/wp-content/uploads/2013/11/19-QhlcPnf.jpg');" class="photo-tile third double"></div>
-            <div style="background: url('http://img.photobucket.com/albums/0903/coyote23/wallpaper-10051.jpg');" class="photo-tile third double"></div>
-            <div style="background: url('http://static.comicvine.com/uploads/original/13/137190/3241351-mario_wallpaper_by_evertonstz-d31aqlm.jpg');" class="photo-tile third double"></div>
-            <div style="background: url('http://orig15.deviantart.net/22ee/f/2012/232/1/8/minimalist_wallpaper__why_not_zoidberg__by_krukmeister-d5bsep6.jpg');" class="photo-tile whole triple"></div>
-            <div style="background: url('http://img.photobucket.com/albums/0903/coyote23/8_bit_heart_by_justicebleeds-d3at0k.jpg');" class="photo-tile half double"></div>
-            <div style="background: url('http://www.wallpaperup.com/uploads/wallpapers/2014/10/21/489352/1ee3d158c4913c5f4d7e505f3e436f89.jpg');" class="photo-tile half double"></div>
-              <div class="photo-tile quarter double">
-                <div style="background: url('https://awesomewallpaper.files.wordpress.com/2012/08/triforce.png');" class="photo-tile whole single"></div>
-                <div style="background: url('https://s-media-cache-ak0.pinimg.com/736x/12/c8/f9/12c8f948cce9b17793f11bd2d34a6c4e.jpg');" class="photo-tile whole single"></div>
-              </div>
-              <div style="background: url('http://www.wallpaperup.com/uploads/wallpapers/2014/01/24/236238/b46fcf8fcba1ae2bc1ecae7510d71509.jpg');" class="photo-tile quarter double"></div>
-              <div style="background: url('https://www.mweb.co.za/DesktopModules/DigArticle/MediaHandler.ashx?portalid=20&moduleid=5259&mediaid=3037&width=665&height=400');" class="photo-tile quarter double"></div>
-              <div style="background: url('https://s-media-cache-ak0.pinimg.com/736x/29/e2/68/29e26859416174d95c9bfcc63afc874c.jpg');" class="photo-tile quarter double"></div>
-              <div style="background: url('http://orig14.deviantart.net/ff5e/f/2012/221/a/c/companion_cube_minimalist_wallpaper_by_krukmeister-d5afc06.jpg');" class="photo-tile third double"></div>
-              <div style="background: url('http://img.photobucket.com/albums/0903/coyote23/tumblr_m5iwppAJnS1qkj7sso7_1280.jpg');" class="photo-tile two-thirds double"></div>
-              <div style="background: url('http://www.wall321.com/thumbnails/detail/20120818/video%20games%20portal%203840x1080%20wallpaper_www.wall321.com_95.jpg');" class="photo-tile whole single"></div>
-              <div style="background: url('http://www.wallpaperup.com/uploads/wallpapers/2014/01/28/240582/d3f26ff12ff66f0010a18180851f487c.jpg');" class="photo-tile two-thirds double"></div>
-              <div style="background: url('http://eskipaper.com/images/gameboy-wallpaper-2.jpg');" class="photo-tile third double"></div>
-              <div style="background: url('https://landonkerth.files.wordpress.com/2012/08/winter2-011.jpg');" class="photo-tile whole triple"></div>
-              <div class="photo-tile three-quarters triple">
-                <div style="background: url('http://img04.deviantart.net/1902/i/2011/252/d/2/space_invaders_wallpaper_by_vellosia-d39td5j.png');" class="photo-tile half single"></div>
-                <div style="background: url('http://wallpaperswa.com/thumbnails/detail/20130601/green%20video%20games%20minimalistic%20mario%20mario%20bros%20super%20mario%20mushrooms%201%20up%20green%20background_wallpaperswa.com_27.jpg');" class="photo-tile half single"></div>
-                <div style="background: url('http://halo5universe.com/wp-content/uploads/2012/06/mini.jpg');" class="photo-tile whole double"></div>
-              </div>
-              <div style="background: url('https://s-ak.buzzfed.com/static/enhanced/web04/2010/6/25/18/enhanced-buzz-11284-1277505819-10.jpg');" class="photo-tile quarter triple"></div>
-              <div class="photo-tile half triple">
-                <div style="background: url('http://vignette2.wikia.nocookie.net/catherinethegame/images/9/96/AW8GAetCEAIkHsQ.jpg/revision/latest/scale-to-width-down/1024?cb=20150727235049')" class="photo-tile whole double"></div>
-                <div style="background: url('https://2.bp.blogspot.com/-osUTAZAKDUQ/UEF5_29R3tI/AAAAAAAAFh4/4i0AuXUDF7U/s1600/Pokemon+wallpaper+hd.png')" class="photo-tile whole single"></div>
-              </div>
-              <div class="photo-tile half triple">
-                <div style="background: url('https://33.media.tumblr.com/tumblr_mcgq1xrWsf1r4tiufo1_500.gif')" class="photo-tile half single"></div>
-                <div style="background: url('http://wallpapercave.com/wp/M2lLaVF.png')" class="photo-tile half single"></div>
-                <div style="background: url('http://www.wall321.com/thumbnails/detail/20120307/video%20games%20fallout%201920x1200%20wallpaper_www.wall321.com_79.jpg')" class="photo-tile whole double"></div>
-              </div>
-              <div style="background: url('http://images6.alphacoders.com/591/591158.jpg');" class="photo-tile whole quadruple"></div>
-            </div>
+        <div class="col-md-6 col-xs-12 heightfortyfive" style="background: url('assets/discoverdiamond4.jpg'); background-size:cover;background-position:center bottom;">
+        </div>
+        <div class="col-md-6 col-xs-12 heightfiftyfive" style="background: url('assets/discoverdiamond3.jpg'); background-size:cover; background-position:center bottom;">
         </div>
       </div>
-    </div>-->
+      <div class="row">
+        <div class="col-md-6 col-xs-12 heightfiftyfive hidden-sm-down" style="background: url('assets/discoverdiamond5.jpg'); background-size:cover;">
+        </div>
+        <div class="col-md-6 col-xs-12 heightfortyfive" style="background: url('assets/discoverdiamond6.jpg'); background-size:cover;background-position:center bottom;">
+        </div>
       </div>
 		</section>
 
