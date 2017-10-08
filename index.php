bb1b830a866bf4ba5557e048d2d90eadad70d07b
Katdelorme <katdelorme@yahoo.com>
2017-10-08 16:59:59 -0400

    original state of site. reverted

---
+++ index.php
@@ -0,0 +1,125 @@
+<?php
+include ('./includes/header.html');
+/* PAGE CONTENT STARTS HERE! */
+?>
+
+  <div id="fullpage">
+	<div class="container-fluid">
+
+		<section class="section hometop">
+			<div class="hometopContent middle">
+				<h2>Bringing Fashion To <br/>Your Fingertips</h2>
+				<h4>A Five Star Salon<br/>At A Five Star Hotel </h4>
+				<p>Debuting November 2017 in Tokyo.
+          <!--Nail designs soaring to new heights, <br>where only the sky is the limit.-->
+        </p>
+        <div>
+          <a href="naildesigns" class="btn">View Styles</a>
+          <a href="contact" class="btn">Book Now</a>
+        </div>
+        <div class="row hotelLogo">
+          <div class="col-4" style="background: url('assets/logoShangrilaTokyo.png'); background-size:contain;background-repeat: no-repeat;">
+          </div>
+          <div class="col-4 offset-4" style="background: url('assets/logoChiSpa.png'); background-size:contain;background-repeat: no-repeat;">
+          </div>
+        </div>
+			</div>
+		</section>
+
+		<section class="section">
+			<div class="threeFourth row">
+				<div class="forty col-md-4 col-xs-12">
+					<img src="assets/owners.jpg" alt="Owners: Janica Southwick and Ryutaro Kashima" class="img-fluid" />
+				</div>
+				<div class="sixty col-md-8 col-xs-12">
+					<h2>Two Salonists Coming Together</h2>
+					<p>A story of two stars coming together. Luxury and Ita. JoliNails a superstar in the luxury and fashion nail industry, unites with VenusRico, a rockstar in the manga and anime ITA art nail world.  A new star is born: LUX &#9733; ITA. Nail designs soaring to new heights, where only the sky is the limit.</p>
+					<div class="logosTwo">
+						<img src="assets/logoJoliNails.png" alt="JoliNails logo" />
+						<img src="assets/logoVenusrico.png" alt="VenusRico Logo" />
+					</div>
+				</div>
+			</div>
+			<div class="oneFourth row">
+				<div class="col-md-9 col-xs-12">
+          <h1>Book An Appointment</h1>
+				</div>
+				<div class="col-md-3 col-xs-12">
+					<a href="contact" class="btn">Book Now</a>
+				</div>
+			</div>
+		</section>
+
+		<section  class="section">
+			<div class="row">
+				<div class="thirty col-md-4 col-xs-12">
+				</div>
+				<div class="seventy col-md-8 col-xs-12">
+					<h1>Discover Diamond Nails</h1>
+					<h3 class="line">Now a girl's best friend is always on hand</h3>
+					<div class="textColumn">
+						<p>Take your passion for luxury to the next level with diamonds on your fingertips 24 hours a day. Make a stunning impact in every situation by shining like the star you are with every hand motion and touch you make.</p>
+		 				<p>LUXITA diamonds are hand picked for the ultimate sparkle by the world's top gemologists. Use the diamond over and over again for repeat treatments. And put your mind at ease knowing that there is a warranty for lost or loose stones.</p>
+						<p>So, change the way that you look at diamonds and form a new bond with your best friend on hand. Soar to new heights with LUXITA diamonds. You deserve it.</p>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<section class="section verticalSpace">
+			<div class="columnThree row">
+				<div class="col-md-4 col-xs-12">
+					<h4>Fashion Nail</h4>
+					<p>En vogue patterns and colors. A style that'll look chic at any occasion. Let us bring fashion to your fingertips.</p>
+				</div>
+				<div class="col-md-4 col-xs-12">
+					<h4>Ita nail</h4>
+					<p>Ita - Nail is a specialized Japanese hand-painted art which uses the nail bed as a canvas.</p>
+				</div>
+				<div class="col-md-4 col-xs-12">
+					<h4>Diamond Nail</h4>
+					<p>The ultimate in luxury nails. The beauty of diamonds at the tips of your fingers.</p>
+				</div>
+			</div>
+			<div class="featuredStyle row">
+				<div class="featuredStyleL col-md-5 col-xs-12">
+					<h1>Minimalistic</h1>
+					<p>Simplistic styles that make a sophisticated impact. Nails that cater to every occasion. Discover a design to match your personality.</p>
+					<a href="naildesigns" class="btn">Select Your Style</a>
+				</div>
+				<div class="featuredStyleR col-md-7 col-xs-12">
+				</div>
+			</div>
+		</section>
+
+		<section class="section newsletter basepadding" data-centered="true">
+					<h1>Subscribe</h1>
+					<h5>Be the first to know about our launch</h5>
+					<!-- Begin MailChimp Signup Form -->
+					<div id="mc_embed_signup">
+					<form action="#" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+						<div id="mc_embed_signup_scroll">
+								<input type="text" value="" name="FNAME" class="" id="mce-FNAME" placeholder="First Name">
+								<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL" placeholder="Email Address">
+							<div id="mce-responses" class="clear">
+								<div class="response" id="mce-error-response" style="display:none"></div>
+								<div class="response" id="mce-success-response" style="display:none"></div>
+							</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+							<div style="position: absolute; left: -5000px;"><input type="text" name="b_7aca85dad056373aad529face_d3753f5f82" tabindex="-1" value=""></div>
+							<div class="clear">
+								<input type="submit" value="Stay in the Know" name="subscribe">
+							</div>
+						</div>
+					</form>
+					</div>
+					<!--End mc_embed_signup-->
+		</section>
+	</div><!--container close-->
+	</div><!--fullpage close-->
+
+
+
+<?php
+/* PAGE CONTENT ENDS HERE! */
+require ('./includes/footer.html');
+?>
